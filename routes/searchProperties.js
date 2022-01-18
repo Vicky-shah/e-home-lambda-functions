2fb683d36690ccd35f370bed44732ca0edb291b8
Harsh shah <harsh95@tymk.in>
2022-01-18 20:22:19 +0530

    changes in search property

--- routes/searchProperties.js
+++ routes/searchProperties.js
@@ -8,6 +8,7 @@ const { forEach } = require('../utils/functions')
 
 module.exports = async (event, context, callback) => {
     const pageNumber = event.queryStringParameters && event.queryStringParameters.pageNumber ? event.queryStringParameters.pageNumber : '1';
+    const keyword = event.queryStringParameters && event.queryStringParameters.keyword ? event.queryStringParameters.keyword : '1';
     if (event.queryStringParameters && event.queryStringParameters.pageNumber) {
         delete event.queryStringParameters.pageNumber;
     }
@@ -23,9 +24,8 @@ module.exports = async (event, context, callback) => {
     const header = {
         headers: HOME_JUNCTION_HEADERS
     };
-    console.log('moreQueryStrings',moreQueryStrings);
-    console.info('moreQueryStrings',moreQueryStrings);
-    const attomResponse = await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+
+    const attomResponse = await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(keyword ? keyword : "")}`, header2)
         .then(async res => {
             let listing = [];
             await forEach(res.data.property, async (each) => {
@@ -33,7 +33,7 @@ module.exports = async (event, context, callback) => {
                     const header2 = {
                         headers: ATTOM_DATA_HEADERS
                     };
-                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(keyword ? keyword : "")}`, header2)
                         .then(async ress => {
                             if (ress.data.property && ress.data.property[0]) {
                                 each = { ...each, attomData: ress.data.property[0] }
@@ -60,41 +60,40 @@ module.exports = async (event, context, callback) => {
             return errorResponse();
         })
 
-console.log('attomResponse',attomResponse);
-    if (!attomResponse) {
-        return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
-        .then(async res => {
-            let listing = [];
-            await forEach(res.data.result.listings, async (each) => {
-                listing.push({
-                    id: each.id,
-                    systemId: each.systemId,
-                    market: each.market,
-                    geoType: each.geoType,
-                    coordinates: each.coordinates,
-                    listPrice: each.listPrice,
-                    beds: each.beds,
-                    address: eachpropertiesListing.address,
-                    images: each.images,
-                    baths: each.baths,
-                    lotSize: each.lotSize,
-                    listingType: each.listingType,
-                    daysOnHJI: each.daysOnHJI
-                })
-            });
-            return okResponse({
-                currentPage: pageNumber,
-                totalPages: Math.ceil(res.data.result.total / 50),
-                total: res.data.result.total,
-                listing: listing
-            })
-        }).catch(() => {
-            return errorResponse();
-        })
-    }else{
+    if (attomResponse) {
         return attomResponse;
+    } else {
+        return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
+            .then(async res => {
+                let listing = [];
+                await forEach(res.data.result.listings, async (each) => {
+                    listing.push({
+                        id: each.id,
+                        systemId: each.systemId,
+                        market: each.market,
+                        geoType: each.geoType,
+                        coordinates: each.coordinates,
+                        listPrice: each.listPrice,
+                        beds: each.beds,
+                        address: eachpropertiesListing.address,
+                        images: each.images,
+                        baths: each.baths,
+                        lotSize: each.lotSize,
+                        listingType: each.listingType,
+                        daysOnHJI: each.daysOnHJI
+                    })
+                });
+                return okResponse({
+                    currentPage: pageNumber,
+                    totalPages: Math.ceil(res.data.result.total / 50),
+                    total: res.data.result.total,
+                    listing: listing
+                })
+            }).catch(() => {
+                return errorResponse();
+            })
     }
-    
+
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 }
