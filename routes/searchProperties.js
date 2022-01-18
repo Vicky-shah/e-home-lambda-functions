1147fe23fa0950ef20367306960aa7fa3ad31a33
Harsh shah <harsh95@tymk.in>
2022-01-18 20:53:10 +0530

    changes in search property by address

--- routes/searchProperties.js
+++ routes/searchProperties.js
@@ -8,7 +8,7 @@ const { forEach } = require('../utils/functions')
 
 module.exports = async (event, context, callback) => {
     const pageNumber = event.queryStringParameters && event.queryStringParameters.pageNumber ? event.queryStringParameters.pageNumber : '1';
-    const keyword = event.queryStringParameters && event.queryStringParameters.keyword ? event.queryStringParameters.keyword : '1';
+    const keyword = event.queryStringParameters && event.queryStringParameters.keyword ? event.queryStringParameters.keyword : ' ';
     if (event.queryStringParameters && event.queryStringParameters.pageNumber) {
         delete event.queryStringParameters.pageNumber;
     }
@@ -25,14 +25,20 @@ module.exports = async (event, context, callback) => {
         headers: HOME_JUNCTION_HEADERS
     };
 
-    const attomResponse = await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(keyword ? keyword : "")}`, header2)
+    const header2 = {
+        headers: ATTOM_DATA_HEADERS
+    };
+
+    const attomResponse = true;
+    if (attomResponse) {
+        return await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(keyword ? keyword : "")}`, header2)
         .then(async res => {
             let listing = [];
             await forEach(res.data.property, async (each) => {
                 if (each) {
-                    const header2 = {
-                        headers: ATTOM_DATA_HEADERS
-                    };
+                    // const header2 = {
+                    //     headers: ATTOM_DATA_HEADERS
+                    // };
                     await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(keyword ? keyword : "")}`, header2)
                         .then(async ress => {
                             if (ress.data.property && ress.data.property[0]) {
@@ -60,8 +66,6 @@ module.exports = async (event, context, callback) => {
             return errorResponse();
         })
 
-    if (attomResponse) {
-        return attomResponse;
     } else {
         return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
             .then(async res => {
@@ -97,3 +101,33 @@ module.exports = async (event, context, callback) => {
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 }
+
+return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
+        .then(async res => {
+            let listing = [];
+            await forEach(res.data.result.listings, async (each) => {
+                listing.push({
+                    id: each.id,
+                    systemId: each.systemId,
+                    market: each.market,
+                    geoType: each.geoType,
+                    coordinates: each.coordinates,
+                    listPrice: each.listPrice,
+                    beds: each.beds,
+                    address: each.address,
+                    images: each.images,
+                    baths: each.baths,
+                    lotSize: each.lotSize,
+                    listingType: each.listingType,
+                    daysOnHJI: each.daysOnHJI
+                })
+            });
+            return okResponse({
+                currentPage: pageNumber,
+                totalPages: Math.ceil(res.data.result.total / 50),
+                total: res.data.result.total,
+                listing: listing
+            })
+        }).catch(() => {
+            return errorResponse();
+        })
