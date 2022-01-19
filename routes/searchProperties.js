838d1e2fd0468b1e471fd05a8a6bec5e97f8b742
Harsh Shah <harshm.shah@it-ally-u63.svaapta.local>
2022-01-19 10:51:17 +0530

    update the changes for search property

--- routes/searchProperties.js
+++ routes/searchProperties.js
@@ -8,6 +8,7 @@ const { forEach } = require('../utils/functions')
 
 module.exports = async (event, context, callback) => {
     const pageNumber = event.queryStringParameters && event.queryStringParameters.pageNumber ? event.queryStringParameters.pageNumber : '1';
+    const keyword = event.queryStringParameters && event.queryStringParameters.keyword ? event.queryStringParameters.keyword : ' ';
     if (event.queryStringParameters && event.queryStringParameters.pageNumber) {
         delete event.queryStringParameters.pageNumber;
     }
@@ -28,71 +29,87 @@ module.exports = async (event, context, callback) => {
         headers: ATTOM_DATA_HEADERS
     };
 
-    return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
-        .then(async res => {
-            let listing = [];
-            await forEach(res.data.result.listings, async (each) => {
-                if (each.stdAddress) {
-                    console.log('each', each);
-                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
-                        .then(async ress => {
-                            if (ress.data.property && ress.data.property[0]) {
-                                each = { ...each, attomData: ress.data.property[0] }
-                            }
-                        })
-                        .catch(err => {
-
-                        })
-                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
-                        .then(async ress => {
-                            if (ress.data.property && ress.data.property[0]) {
-                                each = { ...each, attomData: { ...each.attomData, ...ress.data.property[0] } }
-                            }
-                        }).catch(err => {
-
-                        })
-                }
-                listing.push(each)
-            });
-            if (listing) {
-                return okResponse({
-                    listing: listing
-                })
-            } else {
-                return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
-                    .then(async res => {
-                        let listing = [];
-                        await forEach(res.data.result.listings, async (each) => {
-                            listing.push({
-                                id: each.id,
-                                systemId: each.systemId,
-                                market: each.market,
-                                geoType: each.geoType,
-                                coordinates: each.coordinates,
-                                listPrice: each.listPrice,
-                                beds: each.beds,
-                                address: eachpropertiesListing.address,
-                                images: each.images,
-                                baths: each.baths,
-                                lotSize: each.lotSize,
-                                listingType: each.listingType,
-                                daysOnHJI: each.daysOnHJI
-                            })
-                        });
-                        return okResponse({
-                            currentPage: pageNumber,
-                            totalPages: Math.ceil(res.data.result.total / 50),
-                            total: res.data.result.total,
-                            listing: listing
-                        })
-                    }).catch(() => {
-                        return errorResponse();
-                    });
-            }
-
-        }).catch((err) => {
-            return errorResponse();
+    return await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detail?address=${keyword}`, header)
+    .then(async res => {
+        let listing = [];
+        if (res.data.property && res.data.property[0]) {
+            each = { attomData: res.data.property[0] }
+        }
+        listing.push(each);
+        return okResponse({
+            listing: listing
         })
+    })
+    .catch(err => {
+        return errorResponse();
+    });
+
+
+    // return await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detail?address=${moreQueryStrings}`, header)
+    //     .then(async res => {
+    //         let listing = [];
+    //         await forEach(res.data.result.listings, async (each) => {
+    //             if (each.stdAddress) {
+    //                 console.log('each', each);
+    //                 await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+    //                     .then(async ress => {
+    //                         if (ress.data.property && ress.data.property[0]) {
+    //                             each = { ...each, attomData: ress.data.property[0] }
+    //                         }
+    //                     })
+    //                     .catch(err => {
+
+    //                     })
+    //                 await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+    //                     .then(async ress => {
+    //                         if (ress.data.property && ress.data.property[0]) {
+    //                             each = { ...each, attomData: { ...each.attomData, ...ress.data.property[0] } }
+    //                         }
+    //                     }).catch(err => {
+
+    //                     })
+    //             }
+    //             listing.push(each)
+    //         });
+    //         if (listing) {
+    //             return okResponse({
+    //                 listing: listing
+    //             })
+    //         } else {
+    //             return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
+    //                 .then(async res => {
+    //                     let listing = [];
+    //                     await forEach(res.data.result.listings, async (each) => {
+    //                         listing.push({
+    //                             id: each.id,
+    //                             systemId: each.systemId,
+    //                             market: each.market,
+    //                             geoType: each.geoType,
+    //                             coordinates: each.coordinates,
+    //                             listPrice: each.listPrice,
+    //                             beds: each.beds,
+    //                             address: eachpropertiesListing.address,
+    //                             images: each.images,
+    //                             baths: each.baths,
+    //                             lotSize: each.lotSize,
+    //                             listingType: each.listingType,
+    //                             daysOnHJI: each.daysOnHJI
+    //                         })
+    //                     });
+    //                     return okResponse({
+    //                         currentPage: pageNumber,
+    //                         totalPages: Math.ceil(res.data.result.total / 50),
+    //                         total: res.data.result.total,
+    //                         listing: listing
+    //                     })
+    //                 }).catch(() => {
+    //                     return errorResponse();
+    //                 });
+    //         }
+
+    //     }).catch((err) => {
+    //         return errorResponse();
+    //     })
 
 
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
