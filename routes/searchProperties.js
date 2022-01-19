dd5bc76059726f5780d0590f23d8c6e612585b13
Harsh Shah <harshm.shah@it-ally-u63.svaapta.local>
2022-01-19 16:00:48 +0530

    commit search application changes #4

--- routes/searchProperties.js
+++ routes/searchProperties.js
@@ -6,6 +6,8 @@ const { okResponse, errorResponse } = require('../utils/HttpResponse')
 const { HOME_JUNCTION_HEADERS, ATTOM_DATA_HEADERS } = require('../utils/constants')
 const { forEach } = require('../utils/functions')
 
+const lambdaLocal = require("lambda-local");
+
 module.exports = async (event, context, callback) => {
     const pageNumber = event.queryStringParameters && event.queryStringParameters.pageNumber ? event.queryStringParameters.pageNumber : '1';
     const keyword = event.queryStringParameters && event.queryStringParameters.keyword ? event.queryStringParameters.keyword : ' ';
@@ -34,7 +36,7 @@ module.exports = async (event, context, callback) => {
             let listing = [];
             await forEach(res.data.property, async (each) => {
                 if (each.address.oneLine) {
-                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.address.line1 ? each.address.line1 : "")}${(each.address.locality ? ("," + each.address.locality) : "")}${(each.address.countrySubd ? ("," + each.address.countrySubd) : "")}`, header2)
                         .then(async ress => {
                             if (ress.data.property && ress.data.property[0]) {
                                 each = { ...each, attomData: ress.data.property[0] }
@@ -43,7 +45,7 @@ module.exports = async (event, context, callback) => {
                         .catch(err => {
                             console.log('error',err);
                         })
-                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.address.line1 ? each.address.line1 : "")}${(each.address.locality ? ("," + each.address.locality) : "")}${(each.address.countrySubd ? ("," + each.address.countrySubd) : "")}`, header2)
                         .then(async ress => {
                             if (ress.data.property && ress.data.property[0]) {
                                 each = { ...each, attomData: { ...each.attomData, ...ress.data.property[0] } }
@@ -53,45 +55,17 @@ module.exports = async (event, context, callback) => {
                         })
                 }
                 listing.push(each);
-                if (listing.length) {
-                    return okResponse({
-                        listing: listing
-                    })
-                } else {
-                    return await axios.get(`https://slipstream.homejunction.com/ws/listings/search?pageNumber=${pageNumber}&pageSize=50${moreQueryStrings}`, header)
-                        .then(async res => {
-                            let listing = [];
-                            await forEach(res.data.result.listings, async (each) => {
-                                listing.push({
-                                    id: each.id,
-                                    systemId: each.systemId,
-                                    market: each.market,
-                                    geoType: each.geoType,
-                                    coordinates: each.coordinates,
-                                    listPrice: each.listPrice,
-                                    beds: each.beds,
-                                    address: eachpropertiesListing.address,
-                                    images: each.images,
-                                    baths: each.baths,
-                                    lotSize: each.lotSize,
-                                    listingType: each.listingType,
-                                    daysOnHJI: each.daysOnHJI
-                                })
-                            });
-                            return okResponse({
-                                currentPage: pageNumber,
-                                totalPages: Math.ceil(res.data.result.total / 50),
-                                total: res.data.result.total,
-                                listing: listing
-                            })
-                        }).catch(() => {
-                            return errorResponse();
-                        });
-                }
+                return okResponse({
+                    listing: listing
+                })
             })
-        })
-        .catch(err => {
-            return errorResponse();
+        }).catch(err => {
+            return [
+                console.log('error',err),
+                console.info('error',err),
+                console.warn('error',err),
+                errorResponse()
+            ]
         });
 
 
