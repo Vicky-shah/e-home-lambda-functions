03b58d6038c31a83ba9fedb0b00ca91ce1ab58e6
Support Svaapta <support@svaapta-it-ally.com>
2022-01-25 14:37:39 +0530

    Update singleProperty.js
    Reward the code by NR.

--- routes/singleProperty.js
+++ routes/singleProperty.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const { default: axios } = require("axios");
@@ -8,7 +7,6 @@ const { forEach } = require('../utils/functions')
 
 module.exports = async (event, context, callback) => {
     let moreQueryStrings = "";
-    const id = event.queryStringParameters && event.queryStringParameters.id ? event.queryStringParameters.id : ' ';
     if (event.queryStringParameters) {
         Object.keys(event.queryStringParameters).forEach(each => {
             moreQueryStrings += "&" + each + "=" + event.queryStringParameters[each];
@@ -17,16 +15,15 @@ module.exports = async (event, context, callback) => {
     const header = {
         headers: HOME_JUNCTION_HEADERS
     };
-    const header2 = {
-        headers: ATTOM_DATA_HEADERS
-    };
-    // return await axios.get(`https://slipstream.homejunction.com/ws/listings/get?${moreQueryStrings}`, header)
-    return await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detail?attomid=${id}`, header2)
+    return await axios.get(`https://slipstream.homejunction.com/ws/listings/get?${moreQueryStrings}`, header)
         .then(async res => {
             let listing = [];
-            await forEach(res.data.property, async (each) => {
-                if (each.address.line1) {
-                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address1=${(each.address.line1 ? each.address.line1 : "")}&address2=${(each.address.line2 ? each.address.line2 : "")}`, header2)
+            await forEach(res.data.result.listings, async (each) => {
+                if (each.stdAddress) {
+                    const header2 = {
+                        headers: ATTOM_DATA_HEADERS
+                    };
+                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
                         .then(async ress => {
                             if (ress.data.property && ress.data.property[0]) {
                                 each = { ...each, attomData: ress.data.property[0] }
@@ -35,18 +32,10 @@ module.exports = async (event, context, callback) => {
                         .catch(err => {
 
                         })
-                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address1=${(each.address.line1 ? each.address.line1 : "")}&address2=${(each.address.line2 ? each.address.line2 : "")}`, header2)
+                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
                         .then(async ress => {
                             if (ress.data.property && ress.data.property[0]) {
                                 each = { ...each, attomData: { ...each.attomData, ...ress.data.property[0] } }
-                            }
-                        }).catch(err => {
-
-                        })
-                    await axios.get(`https://slipstream.homejunction.com/ws/listings/get&id=${id}&images=true&market=cjmls`, header)
-                        .then(async res => {
-                            if (res.data.result.listings && res.data.result.listings[0]) {
-                                each = { ...each, slipStreem: { ...each.attomData, ...res.data.result.listings[0]} }
                             }
                         }).catch(err => {
 
