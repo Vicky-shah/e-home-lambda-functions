eaa7f5e5ade6380b5bbbe61f1c3b1916375eafdb
Support Svaapta <support@svaapta-it-ally.com>
2022-02-01 19:09:05 +0530

    Update singleProperty.js
    revert back code change of atom and slipstream api

--- routes/singleProperty.js
+++ routes/singleProperty.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const { default: axios } = require("axios");
@@ -16,38 +15,75 @@ module.exports = async (event, context, callback) => {
     const header = {
         headers: HOME_JUNCTION_HEADERS
     };
+    return await axios.get(`https://slipstream.homejunction.com/ws/listings/get?${moreQueryStrings}`, header)
+        .then(async res => {
+            let listing = [];
+            await forEach(res.data.result.listings, async (each) => {
+                if (each.stdAddress) {
+                    const header2 = {
+                        headers: ATTOM_DATA_HEADERS
+                    };
+                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+                        .then(async ress => {
+                            if (ress.data.property && ress.data.property[0]) {
+                                each = { ...each, attomData: ress.data.property[0] }
+                            }
+                        })
+                        .catch(err => {
 
-return await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
-.then(async res => {
-    let listing = [];
-    await forEach(res.data.property, async (each) => {
-        if (each) {
-            const header2 = {
-                headers: ATTOM_DATA_HEADERS
-            };
-            await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
-                .then(async ress => {
-                    if (ress.data.property && ress.data.property[0]) {
-                        each = { ...each, attomData: ress.data.property[0] }
-                    }
-                })
-                .catch(err => {
+                        })
+                    await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+                        .then(async ress => {
+                            if (ress.data.property && ress.data.property[0]) {
+                                each = { ...each, attomData: { ...each.attomData, ...ress.data.property[0] } }
+                            }
+                        }).catch(err => {
 
-                })
-                await axios.get(`https://slipstream.homejunction.com/ws/listings/get?${moreQueryStrings}`, header2)
-                .then(async ress => {
-                    if (ress.data.result.listings && ress.data.result.listings) {
-                        each = { ...each, attomData: { ...each.attomData, ...ress.data.property[0] } }
-                    }
-                }).catch(err => {
+                        })
+                }
+                listing.push(each)
+            });
+            return okResponse({
+                listing: listing
+            })
+        }).catch((err) => {
+            return errorResponse();
+        })
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+}
 
-                })
-        }
-        listing.push(each)
-    });
-    return okResponse({
-        listing: listing
-    })
-}).catch((err) => {
-    return errorResponse();
-})
+// return await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+// .then(async res => {
+//     let listing = [];
+//     await forEach(res.data.property, async (each) => {
+//         if (each) {
+//             const header2 = {
+//                 headers: ATTOM_DATA_HEADERS
+//             };
+//             await axios.get(`https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/expandedhistory?address=${(each.stdAddress.deliveryLine ? each.stdAddress.deliveryLine : "")}${(each.stdAddress.city ? ("," + each.stdAddress.city) : "")}${(each.stdAddress.state ? ("," + each.stdAddress.state) : "")}`, header2)
+//                 .then(async ress => {
+//                     if (ress.data.property && ress.data.property[0]) {
+//                         each = { ...each, attomData: ress.data.property[0] }
+//                     }
+//                 })
+//                 .catch(err => {
+
+//                 })
+//                 await axios.get(`https://slipstream.homejunction.com/ws/listings/get?${moreQueryStrings}`, header2)
+//                 .then(async ress => {
+//                     if (ress.data.result.listings && ress.data.result.listings) {
+//                         each = { ...each, attomData: { ...each.attomData, ...ress.data.property[0] } }
+//                     }
+//                 }).catch(err => {
+
+//                 })
+//         }
+//         listing.push(each)
+//     });
+//     return okResponse({
+//         listing: listing
+//     })
+// }).catch((err) => {
+//     return errorResponse();
+// })
